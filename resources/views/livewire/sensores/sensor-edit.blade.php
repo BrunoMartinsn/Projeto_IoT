7af0585d971f28ebb0f937d982ac41ef28a7d4c0
bruno <brurro67@gmail.com>
2025-08-28 14:57:18 -0300

    alteração do Status

--- resources/views/livewire/sensores/sensor-edit.blade.php
+++ resources/views/livewire/sensores/sensor-edit.blade.php
@@ -74,13 +74,22 @@
                                              @error('descricao')
                                                     <span class="text-danger small">{{ $message }}</span>
                                              @enderror
-                                            </div>
                                           
-                                          <div class="form-check form-switch">
-                            <input class="form-check-input" type="checkbox" role="switch" id="status"
-                                wire:model.defer="status">
-                            <label class="form-check-label" for="status">Status do ambiente</label>
-                        </div>
+                                       <div class="mb-3">
+            
+                <label for="status">Status</label>
+               
+            </span>
+
+            <select class="form-select" aria-label="default-select example"@error('status') is-invalid @enderror
+                id="status" wire:model.defer="status" placeholder="">
+                <option hidden></option>
+                <option value="1">True</option>
+                <option value="0">False</option>
+            </select>
+            @error('status')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
                 
                
               
