ed8db0395a0d529fe234e4b06d85432e8b37ebea
bruno <brurro67@gmail.com>
2025-09-16 15:26:27 -0300

    c

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RegistroController;
 use App\Livewire\Dashboard;
 use App\Livewire\Sensores\SensorCreate;
 use App\Livewire\Sensores\SensorEdit;
@@ -8,8 +9,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', Dashboard::class);
 
+<<<<<<< Updated upstream
 Route::prefix('sensor')->group(function () {
  Route::get('/create', SensorCreate::class)->name('sensor-create');
  Route::get('/index', SensorIndex::class)->name('sensor-index');
   Route::get('/edit/{id}', SensorEdit::class)->name('sensor-edit');
 });
+=======
+>>>>>>> Stashed changes
